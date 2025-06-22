030ef49733b3b2c0c3c490f43a5922a5f71f6844
rzotr2 <pavel.lukin126@gmail.com>
2025-06-22 17:26:37 +0200

    SMALL FIX - fixed translations for Not Found Page.

--- frontend/src/components/NotFoundPage.tsx
+++ frontend/src/components/NotFoundPage.tsx
@@ -1,6 +1,9 @@
 import { Link } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
 
 export const NotFoundPage = () => {
+    const { t } = useTranslation();
+
     return (
         <section className="bg-white h-[calc(100vh-56px-64px-32px)] flex items-center">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
@@ -9,18 +12,17 @@ export const NotFoundPage = () => {
                         404
                     </h1>
                     <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
-                        Something's missing.
+                        {t("notFound.title")}
                     </p>
                     <p className="mb-4 text-lg font-light text-gray-500 ">
-                        Sorry, we can't find that page. You'll find lots to explore on the home
-                        page.{" "}
+                        {t("notFound.description")}
                     </p>
                     <Link
                         to="/"
                         from="/"
                         className="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                     >
-                        Back to Homepage
+                        {t("notFound.backToHome")}
                     </Link>
                 </div>
             </div>
